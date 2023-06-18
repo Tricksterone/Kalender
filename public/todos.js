179da199515bf87e48dfa53347ee10e788779ad0
Graveknee <simoncarlstein@live.se>
2023-06-18 11:52:48 +0200

    Added skeleton of the form

--- public/todos.js
+++ public/todos.js
@@ -1,3 +1,10 @@
+const form = document.getElementById("form"),
+title = document.getElementById("titleInput"),
+errorMsgTitle = document.getElementById("error-message-title"),
+errorMsgDate = document.getElementById("error-message-date")
+dateInput = document.getElementById("dateInput"),
+description = document.getElementById("textInput");
+
 function initTodosList() {
   initSideBar();
   // eventlisteners för knappar
@@ -13,3 +20,30 @@ function initSideBar() {
     sidebar.classList.toggle("active");
   };
 }
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  formValidation();
+})
+
+const formValidation = () => {
+  if(title.value === ""){
+    errorMsgTitle.innerHTML = "Du måste skriva in en titel";
+  }
+  else if(dateInput.value === ""){
+    errorMsgDate.innerHTML = "Du måste välja ett datum!"
+  }
+  else{
+    errorMsgTitle.innerHTML = "";
+    errorMsgDate.innerHTML = "";
+    acceptData();
+  }
+}
+
+const data = {}
+const acceptData = () => {
+  data["text"] = title.value;
+  data["day"] = dateInput.value;
+  data["description"] = description.value;
+  console.log(data);
+};
