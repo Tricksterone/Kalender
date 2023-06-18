067790a59783381556d3e21ec1e3e6e417d61f14
Graveknee <simoncarlstein@live.se>
2023-06-18 15:43:02 +0200

    Adding task works

--- public/todos.js
+++ public/todos.js
@@ -3,7 +3,9 @@ title = document.getElementById("titleInput"),
 errorMsgTitle = document.getElementById("error-message-title"),
 errorMsgDate = document.getElementById("error-message-date")
 dateInput = document.getElementById("dateInput"),
-description = document.getElementById("textInput");
+description = document.getElementById("textInput"),
+createdTodo = document.getElementById("created-todo"),
+todos = document.getElementById("show-todos");
 
 function initTodosList() {
   initSideBar();
@@ -32,18 +34,33 @@ const formValidation = () => {
   }
   else if(dateInput.value === ""){
     errorMsgDate.innerHTML = "Du måste välja ett datum!"
+    errorMsgTitle.innerHTML = "";
   }
   else{
     errorMsgTitle.innerHTML = "";
     errorMsgDate.innerHTML = "";
-    acceptData();
+    collectData();
   }
 }
 
+// Samlar in input från formuläret för att spara i data
 const data = {}
-const acceptData = () => {
+const collectData = () => {
   data["text"] = title.value;
   data["day"] = dateInput.value;
   data["description"] = description.value;
-  console.log(data);
+  console.log(data); // För testandets skull
+  createdTodoMessage();
+  ListTodos();
+  // Funktion att rensa formuläret/gå tillbaka till kalender-vyn?
+};
+
+const createdTodoMessage = () => {
+  createdTodo.innerHTML = "Du har lagt till en to-do"
+};
+
+const ListTodos = () => {
+  todos.innerHTML += `<div><span>${data.text}</span>
+  <p>Datum: ${data.day}</p><br>
+  <p>Beskrivning: ${data.description}</p></div`
 };
