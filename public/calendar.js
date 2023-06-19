33cf288db1fdbfb6f6b2cd0d2eca3c4ced17c1cd
Elin Nyman <elin.nyman87@gmail.com>
2023-06-19 19:51:26 +0200

    need to resolve some issues in todo.js

--- public/calendar.js
+++ public/calendar.js
@@ -130,15 +130,25 @@ class Calendar {
   }
 
   highlightCurrentDay() {
+    // Date konstruktor
     const currentDate = new Date();
-    const currentDay = currentDate.getDate();
-    const dayElements = this.calendarDays.getElementsByClassName("day");
 
+    // Hämtar dag, månad, år från nuvarande datum med getDate();
+    const currentDay = currentDate.getDate();
+    // Lagt till månad/år med då vi förmodligen kommer behöva det
+
+    // Letar efter element(en) representerar nuvarande dag
+    const calendar = document.getElementById("calendar");
+    const dayElements = calendar.getElementsByClassName("day");
+
+    // Loopar igenom dag elementen och kollar ifall datumet matchar nuvarande datum
+    // ja det är en for loop Elin ;)
     for (let i = 0; i < dayElements.length; i++) {
       const dayElement = dayElements[i];
-      const day = parseInt(dayElement.querySelector(".date").textContent);
+      const day = parseInt(dayElement.innerText);
 
       if (day === currentDay) {
+        // CSS styling för highlighten
         dayElement.style.backgroundColor = "#03204e";
         dayElement.style.color = "white";
       }
