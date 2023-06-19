fbc7b76d928b3880b4d962a9902d767d4ef9cc65
Elin Nyman <112871777+Fiffigelin@users.noreply.github.com>
2023-06-19 12:46:52 +0200

    Form is up but the functions doesnt work (#48)

--- public/todos.js
+++ public/todos.js
@@ -1,11 +1,34 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const createTodoBtn = document.querySelector("#createTodoLink");
+  const todoForm = document.getElementById("todoForm");
+  const otherElements = document.querySelectorAll(
+    "main.item-b > div.main-container > div.left > div.sidebar > div.top > ul > li:not(:first-child)"
+  );
+
+  createTodoBtn.addEventListener("click", function (event) {
+    event.preventDefault();
+    todoForm.classList.toggle("hidden");
+
+    // Toggle visibility of other elements
+    for (let element of otherElements) {
+      element.classList.toggle("hidden");
+    }
+  });
+});
+
 const form = document.getElementById("form"),
-title = document.getElementById("titleInput"),
-errorMsgTitle = document.getElementById("error-message-title"),
-errorMsgDate = document.getElementById("error-message-date")
-dateInput = document.getElementById("dateInput"),
-description = document.getElementById("textInput"),
-createdTodo = document.getElementById("created-todo"),
-todos = document.getElementById("show-todos");
+  title = document.getElementById("titleInput"),
+  errorMsgTitle = document.getElementById("error-message-title"),
+  errorMsgDate = document.getElementById("error-message-date"),
+  dateInput = document.getElementById("dateInput"),
+  description = document.getElementById("textInput"),
+  createdTodo = document.getElementById("created-todo"),
+  todos = document.getElementById("show-todos");
+
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+  formValidation();
+});
 
 function initTodosList() {
   initSideBar();
@@ -17,70 +40,75 @@ function initTodosList() {
 function initSideBar() {
   let btn = document.querySelector("#btn");
   let sidebar = document.querySelector(".sidebar");
+  let todoForm = document.getElementById("todoForm");
 
   btn.onclick = function () {
     sidebar.classList.toggle("active");
   };
+
+  let createTodoLink = document.querySelector('a[href="to-do.html"]');
+  createTodoLink.addEventListener("click", (event) => {
+    event.preventDefault();
+    todoForm.classList.toggle("hidden");
+  });
 }
 
-form.addEventListener('submit', (event) => {
+const submitButton = document.querySelector("#form button[type='submit']");
+submitButton.addEventListener("click", function (event) {
   event.preventDefault();
   formValidation();
-})
+});
 
 const formValidation = () => {
-  if(title.value === ""){
+  if (title.value === "") {
     errorMsgTitle.innerHTML = "Du måste skriva in en titel";
-  }
-  else if(dateInput.value === ""){
-    errorMsgDate.innerHTML = "Du måste välja ett datum!"
+  } else if (dateInput.value === "") {
+    errorMsgDate.innerHTML = "Du måste välja ett datum!";
     errorMsgTitle.innerHTML = "";
-  }
-  else{
+  } else {
     errorMsgTitle.innerHTML = "";
     errorMsgDate.innerHTML = "";
     collectData();
+    resetForm();
   }
-}
+};
 
 // Samlar in input från formuläret för att spara i data
-let TodoCollection = []
+let TodoCollection = [];
 
-const collectData = () => {
+function collectData() {
   TodoCollection.push({
     title: title.value,
     day: dateInput.value,
     description: description.value,
   });
-  // sparar i LocalStorage
   localStorage.setItem("data", JSON.stringify(TodoCollection));
-  console.log(TodoCollection); // För testandets skull
   createdTodoMessage();
-};
+}
 
-const createdTodoMessage = () => {
+function createdTodoMessage() {
   createdTodo.innerHTML = "Du har lagt till en to-do";
   ListTodos();
   resetForm();
-  // Gå tillbaka till kalendervyn-funktion?
-};
+  todoForm.classList.add("hidden"); // Hide the form
+}
 
-const ListTodos = () => {
+function ListTodos() {
   todos.innerHTML = "";
-  TodoCollection.map((x, y)=>{
-    return (  todos.innerHTML += `
-    <div id=${y}>
-    <span>${x.title}</span><br>
-    <p>${x.day}</p><br>
-    <p>Beskrivning:</p>
-    <span>${x.description}</span><br>
-    <i onClick="deleteTodo(this)" class="fa-solid fa-trash" style="color: #3d4657;"></i>
-    <i onClick="editTodo(this)" class="fa-solid fa-pen-to-square" style="color: #2b384f;"></i>
-    </div>`)
-  })
-};
+  TodoCollection.map(function (x, y) {
+    return (todos.innerHTML += `
+      <div id=${y}>
+        <span>${x.title}</span><br>
+        <p>${x.day}</p><br>
+        <p>Beskrivning:</p>
+        <span>${x.description}</span><br>
+        <i onClick="deleteTodo(this)" class="fa-solid fa-trash" style="color: #3d4657;"></i>
+        <i onClick="editTodo(this)" class="fa-solid fa-pen-to-square" style="color: #2b384f;"></i>
+      </div>`);
+  });
+}
 
-const resetForm =  () => {
+const resetForm = () => {
   title.value = "";
   dateInput.value = "";
   description.value = "";
@@ -102,9 +130,26 @@ let deleteTodo = (td) => {
   td.parentElement.remove();
   TodoCollection.splice(td.parentElement, 1);
   localStorage.setItem("data", JSON.stringify(TodoCollection));
-}
+};
 
 (() => {
   TodoCollection = JSON.parse(localStorage.getItem("data")) || [];
   ListTodos();
 })();
+
+// Toggle form visibility
+const createTodoLink = document.getElementById("createTodoLink");
+const todoForm = document.getElementById("todoForm");
+const otherElements = document.querySelectorAll(
+  "main.item-b > div.main-container > div.left > div.sidebar > div.top > ul > li:not(:first-child)"
+);
+
+createTodoLink.addEventListener("click", function (event) {
+  event.preventDefault();
+  todoForm.classList.toggle("hidden");
+
+  // Toggle visibility of other elements
+  for (let element of otherElements) {
+    element.classList.toggle("hidden");
+  }
+});
